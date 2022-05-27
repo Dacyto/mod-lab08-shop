c64ded4d4d5ca0cfc5c3de124e1a20c8c5c9a69e
Dacyto <55278341+Dacyto@users.noreply.github.com>
2022-05-27 13:19:23 +0300

    Update tests.cpp

--- test/tests.cpp
+++ test/tests.cpp
@@ -1,5 +1,17 @@
-// Copyright 2021 GHA Test Team
+// Copyright 2022 GHA Test Team
 #include <gtest/gtest.h>
-#include "task1.h"
-#include "task2.h"
-#include "task3.h"
+#include "../include/task.h"
+
+TEST(TEST1, test1) {
+    int cashboxes = 3;
+    int intensity = 40;
+    int speed = 15;
+    int averageProducts = 3;
+    int clients = 3;
+
+    Shop supermarket(cashboxes, intensity, speed, averageProducts, clients);
+    supermarket.startShopping();
+
+    int result = supermarket.getAmountOfNotServedClients();
+    ASSERT_EQ(result, 0);
+}
