885f8cf2e1fc28d2d79a3d10fd1fbdbdc7c6d450
Dacyto <55278341+Dacyto@users.noreply.github.com>
2022-05-27 13:11:09 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -3,9 +3,22 @@
 
 int main()
 {
-  int cashboxes = 2;
+    int cashboxes = 2;
     int intensity = 30;
     int speed = 10;
     int averageProducts = 3;
     int clients = 4;
+  
+    Shop supermarket(cashboxes, intensity, speed, averageProducts, clients);
+    supermarket.startShopping();
+
+    std::cout << "Число необслуженных клиентов: " << supermarket.getAmountOfServedClients() << std::endl;
+    std::cout << "Число обслуженных клиентов: " << supermarket.getAmountOfNotServedClients() << std::endl;
+    std::cout << "Средняя длина очереди: " << supermarket.getAverageQueueLength() << std::endl;
+    std::cout << "Среднее время пребывания покупателя в очереди и на кассе: " << supermarket.getAverageClientTime() << std::endl;
+    std::cout << "Среднее время работы кассы: " << supermarket.getAverageCashboxWorktime() << std::endl;
+    std::cout << "Среднее время простоя кассы: " << supermarket.getAverageCashboxDowntime() << std::endl;
+    std::cout << "Вероятность отказа: " << supermarket.getPrej() << std::endl;
+    std::cout << "относительная пропускная способность Q: " << supermarket.getQ() << std::endl;
+    std::cout << "абсолютная пропускная способность A: " << supermarket.getA() << std::endl;
 }
